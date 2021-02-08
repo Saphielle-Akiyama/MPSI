7b261bdfda1d540ffb4e6bea7b562c6a72906250
Saphielle Akiyama <saphielle.akiyama@gmail.com>
2021-02-08 16:44:31 +0100

    CARRE OWO

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import numpy as np
+import PIL
 import matplotlib.image as img
 from matplotlib import pyplot as plt
 
-image = img.imread("lena.jpeg", format="RGB")
+image_data = PIL.Image.open("carré.png")
+image = np.array(image_data)
+
 
 width, height, _ = image.shape
 
 for x in range(width - 2):
     for y in range(height - 2):
-        pixel = image[x:x + 3, y:y + 3]
+        pixel = image[x:x + 3, y:y + 3, 0]
         print(pixel)
