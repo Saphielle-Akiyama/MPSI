41384e5b07ed48d40dffa8f65981ba426039cb1a
Saphielle Akiyama <saphielle.akiyama@gmail.com>
2021-03-08 16:34:46 +0100

    Some more memes

--- test.py
+++ test.py
@@ -1,17 +1,7 @@
 import numpy as np
+from PIL import Image
 
-arr = np.zeros((4, 4), dtype=int)
+image = np.array(Image.open("ballon.png"))
 
-g, d = arr.shape
 
-for i in range(g):
-    for j in range(d):
-        arr[i, j] = i + j
 
-print(arr)
-
-print("-" * 50)
-
-for x in range(g - 2):
-    for y in range(d - 2):
-        print(arr[x:x + 3, y:y + 3])
